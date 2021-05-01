6d176a07735b37599d055c3305273a35267f7a64
Pervage Alam <pervage.alam@thoughtworks.com>
2021-05-01 20:20:05 +0530

    Update performance-test.js

--- performance-test.js
+++ performance-test.js
@@ -1,17 +1,27 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
-    
+export let options = { 
   stages: [
-    { duration: '30s', target: 100 },
-    { duration: '1m30s', target: 200 },
+    { duration: '30s', target: 50 },
+    { duration: '1m30s', target: 50 },
     { duration: '20s', target: 0 },
   ],
 };
 
 export default function () {
-  let res = http.get('https://httpbin.org/');
+  var url = 'https://dev-dot-team-service-non-prod.appspot.com/teams/';
+
+  var params = {
+    headers: {
+      'x-api-key' : 'aVLcrQJsO9sWxUzG5TnVF',
+      'OKTA-scp': 'manage-teams-dev',
+      'OKTA-cid' : '0oaxvxs33bUWnwP1J0h7',
+    },
+  };
+
+  let res = http.get(url,params);
+
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
