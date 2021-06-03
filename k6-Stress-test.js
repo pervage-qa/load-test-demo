41fb6074b5c09be6eff778175b30dc7d98bc7f08
Pervage Alam <pervage.alam@thoughtworks.com>
2021-06-03 10:50:46 +0530

    main

--- k6-Stress-test.js
+++ k6-Stress-test.js
@@ -11,10 +11,15 @@ export const options = {
       executor: 'ramping-vus',
       startVUs: 0,
       stages: [
-        { duration: '10s', target: 5 },
-        { duration: '10s', target: 10 },
-        { duration: '10s', target: 5 },
-        { duration: '5s', target: 0 },
+        { duration: '2m', target: 1000 }, // below normal load
+        { duration: '5m', target: 1000 },
+        { duration: '2m', target: 2000 }, // normal load
+        { duration: '5m', target: 2000 },
+        { duration: '2m', target: 3000 }, // around the breaking point
+        { duration: '5m', target: 3000 },
+        { duration: '2m', target: 4000 }, // beyond the breaking point
+        { duration: '5m', target: 4000 },
+        { duration: '10m', target: 0 }, // scale down. Recovery stage.
       ],
       gracefulRampDown: '0s',
     },
@@ -22,17 +27,9 @@ export const options = {
 };
 
 export default function () {
-  var url = 'https://dev-dot-team-service-non-prod.appspot.com/teams/';
+  var url = 'https://gorest.co.in/public-api/users';
 
-  var params = {
-    headers: {
-      'x-api-key' : 'aVLcrQJsO9sWxUzG5TnVF',
-      'OKTA-scp': 'manage-teams-dev',
-      'OKTA-cid' : '0oaxvxs33bUWnwP1J0h7',
-    },
-  };
-
-  const result = http.get(url,params);
+  const result = http.get(url);
   check(result, { 'status was 200': (r) => r.status == 200 });
   failures.add(result.status !== 200);
   sleep(3);
